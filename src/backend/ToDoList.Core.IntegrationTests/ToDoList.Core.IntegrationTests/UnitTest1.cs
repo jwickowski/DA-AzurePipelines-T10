649607a31e43f595e799548ac1c37a1182132ddf
Jerzy Wickowski <kontakt@jerzywickowski.pl>
2019-07-04 20:20:36 +0200

    add backend integration test

--- src/backend/ToDoList.Core.IntegrationTests/ToDoList.Core.IntegrationTests/UnitTest1.cs
+++ src/backend/ToDoList.Core.IntegrationTests/ToDoList.Core.IntegrationTests/UnitTest1.cs
@@ -1,4 +1,3 @@
-using System;
 using System.IO;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
@@ -13,29 +12,43 @@ namespace ToDoList.Core.IntegrationTests
         [Fact]
         public void CheckCreatorAndReader()
         {
-            var cs = GetTestConnectionString();
-            var optionsBuilder = new DbContextOptionsBuilder<ToDoListContext>();
-            optionsBuilder.UseSqlServer(cs);
-            using (var context = new ToDoListContext())
+            using (var context = PrepareContext())
             {
+                CleanDatabase(context);
                 var storageSaver = new DbStorage(context);
                 var creator = new ListItemCreator(storageSaver);
-                var item = creator.Add("a list item");
-                
+
+                creator.Add("a list item");
+
+                var item = context.ToDoListItems.Single();
+                Assert.False(item.IsDone);
+                Assert.Equal("a list item", item.Name);
             }
         }
 
+        private ToDoListContext PrepareContext()
+        {
+            var cs = GetTestConnectionString();
+            var optionsBuilder = new DbContextOptionsBuilder<ToDoListContext>();
+            optionsBuilder.UseSqlServer(cs);
+            return new ToDoListContext(optionsBuilder.Options);
+        }
+        
         private string GetTestConnectionString()
         {
             var projectDir = Directory.GetCurrentDirectory();
             var configPath = Path.Combine(projectDir, "appsettings.json");
             var configJson = File.ReadAllText(configPath);
             var resource = JObject.Parse(configJson);
-            var csss = resource["ConnectionStrings"];
             var connectionStinrg = resource["ConnectionStrings"];
             var cs = connectionStinrg["ToDoListDatabase"];
             var aa = cs.Value<string>();
             return aa;
+        }
+        
+        private void CleanDatabase(ToDoListContext context)
+        {
+            context.Database.ExecuteSqlCommand("DELETE ToDoListItems");
         }
     }
 }
